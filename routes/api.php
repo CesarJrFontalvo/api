3afe45ecd0c149d48bc46ebe7d1c07be84bd4b1c
CesarFontalvo-trabajo <juniorfontalvo5@gmail.com>
2021-06-28 14:46:08 -0500

    modificó ruta V1

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,8 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 // Version 1
 Route::apiResource('v1/posts', PostV1::class)
-    ->only(['index' , 'show', 'destroy']);
-    //->middleware('auth::sanctum');
+    ->only(['index' , 'show', 'destroy'])
+    ->middleware('auth::sanctum');
 
 // Version 2
 Route::apiResource('v2/posts', PostV2::class)
